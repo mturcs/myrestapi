0a3f9a556c05621372ed4ba4b1ea7c7bf63ac806
Mátyás Turcsányi <turcsanyi.matyas@gmail.com>
2021-12-03 14:31:09 +0100

    delete app

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('app');
 var debug = require('debug')('express-locallibrary-tutorial:server');
 var http = require('http');
 
